4819c005334bd6abf46ec100c14dd189985d0c5b
Michael Pine <michaelpine@Michaels-Air.lan>
2023-11-22 17:10:39 -0500

    update img src

--- src/components/weatherApp.jsx
+++ src/components/weatherApp.jsx
@@ -26,11 +26,11 @@ function WeatherApp() {
   }
 
   const weatherIcons = {
-    "rain": '../../rain.png',
-    "cloudy": '../../clouds.png',
-    "partly-cloudy-day": '../../partial-cloud.png',
-    "clear-day": '../../sun.png',
-    "snow": '../../snowy.png',
+    "rain": './portfolio/rain.png',
+    "cloudy": './portfolio/clouds.png',
+    "partly-cloudy-day": './portfolio/partial-cloud.png',
+    "clear-day": './portfolio/sun.png',
+    "snow": './portfolio/snowy.png',
   }
 
 
